d496df16a4fbac02301af5134fd6e9584e39f86f
Vadim Kasatkin <chack.k@mail.ru>
2019-08-13 23:45:04 +0400

    create HW 6

--- models/ShopDB.php
+++ models/ShopDB.php
@@ -1,9 +1,21 @@
 <?php
 
 require '../vendor/autoload.php';
+require  '../config/orm_settings.php';
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 use Illuminate\Database\Schema\Blueprint;
+
+Capsule::schema()->dropIfExists('categories');
+
+Capsule::schema()->create('categories', function (Blueprint $table) {
+    $table->increments('id');
+    $table->string('name');
+    $table->mediumText('description');
+    $table->timestamps();
+});
+
+Capsule::schema()->dropIfExists('products');
 
 Capsule::schema()->create('products', function (Blueprint $table) {
     $table->increments('id');
@@ -11,13 +23,6 @@ Capsule::schema()->create('products', function (Blueprint $table) {
     $table->mediumText('description');
     $table->float('price')->unsigned();
     $table->integer('category_id')->unsigned();
-    $table->foreign('category_id')->references('id')->on('categories');
-    $table->timestamps();
-});
-
-Capsule::schema()->create('categories', function (Blueprint $table) {
-    $table->increments('id');
-    $table->string('name');
-    $table->mediumText('description');
+    $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
     $table->timestamps();
 });
